550a59ab065d2a6bd94475954ec4a4b3c217d0bb
JonasP <visokiems@gmail.com>
2022-10-29 10:29:03 +0300

    code added

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(number1, number2) {
+  this.number1 = number1;
+  this.number2 = number2;
+  this.sum = function () {
+    return this.number1 + this.number2;
+  };
+  this.subtraction = function (number1, number2) {
+    return this.number1 - this.number2;
+  };
+  this.multiplication = function (number1, number2) {
+    return this.number1 * this.number2;
+  };
+  this.division = function (number1, number2) {
+    return this.number1 / this.number2;
+  };
+}
+
+let numbers1 = new Calculator(100, 20);
+let numbers2 = new Calculator(500, 25);
+
+console.log(numbers1.sum());
+console.log(numbers1.subtraction());
+console.log(numbers1.multiplication());
+console.log(numbers1.division());
+
+console.log(numbers2.sum());
+console.log(numbers2.subtraction());
+console.log(numbers2.multiplication());
+console.log(numbers2.division());
